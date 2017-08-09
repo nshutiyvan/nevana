75d3296a45d0fed4bede94f152749382d18b74bc
Lenovo-Yoga\vectaa <nshutiyvan@gmail.com>
2017-08-09 12:09:33 +0200

    changed the gitignore and working on the email service

--- djangp_og/settings.py
+++ djangp_og/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f4-muw_kfk+cont0dtrr0&p^&oju(g2w%b4g5lryxo!hhs8pts'
+SECRET_KEY =config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["djangpog.herokuapp.com"]
+ALLOWED_HOSTS = ["djangpog.herokuapp.com","*"]
 
 
 # Application definition
@@ -85,7 +86,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
