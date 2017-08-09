878d43ad69440c78b25ad84440c55fa85cace7e6
Lenovo-Yoga\vectaa <nshutiyvan@gmail.com>
2017-08-09 12:12:25 +0200

    changed allowed host

--- djangp_og/settings.py
+++ djangp_og/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY =config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ["djangpog.herokuapp.com","*"]
+ALLOWED_HOSTS = ["djangpog.herokuapp.com"]
 
 
 # Application definition
